de151eb6f8db466443aee3c5986aa88112f1af53
vidit jain <jainvidit365vidit@gmail.com>
2025-04-04 15:30:25 +0530

    changes in the prisma

--- app/api/AiOutput/route.ts
+++ app/api/AiOutput/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import prisma from "../../../lib/prisma.js"
+import prisma from "../../../lib/prisma"
 import { getAuth } from "@clerk/nextjs/server";
 
 // Create AiOutput
